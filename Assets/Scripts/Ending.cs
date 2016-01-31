cd7b062962c13315f9301012fab3278febe57021
Alix Ollivier <aollivier82@gmail.com>
2016-01-31 15:31:04 -0800

    Made Ending

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Ending : MonoBehaviour {
+
+    GameObject player;
+    Quaternion playerRotation;
+    Vector3 playerPosition;
+
+    public Camera cam;
+    public Text text;
+
+    bool isDone = false;
+    public float speed;
+
+    public FadeInOut fader;
+
+    void Start()
+    {
+        fader = GameObject.FindObjectOfType<FadeInOut>();
+    }
+
+    void FixedUpdate()
+    {
+        if (isDone == true)
+        {
+            print("TEA");
+            cam.transform.Translate(new Vector3(0, speed));
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "FPSController")
+        {
+            player = other.gameObject;
+            playerPosition = player.transform.position;
+            playerRotation = player.transform.rotation;
+            player.SetActive(false);
+            cam.transform.position = playerPosition;
+            cam.transform.rotation = playerRotation;
+            text.enabled = true;
+            isDone = true;
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            
+
+        }
+    }
+}
